7403ed5f46520935bdf9b5699ec9e2d59648fe35
Adam <adammoore118@outlook.com>
2021-11-29 11:58:05 -0500

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1 = Taxi.create()
+2 = Taxi.create()
+3 = Taxi.create()
+4 = Taxi.create()
+5 = Taxi.create()
+
+Ride.create(taxi_id = 1, passenger_id = 5)
+Ride.create(taxi_id = 2, passenger_id = 4)
+Ride.create(taxi_id = 3, passenger_id = 3)
+Ride.create(taxi_id = 4, passenger_id = 2)
+Ride.create(taxi_id = 5, passenger_id = 1)
+
+
+1=Passenger.create()
+2=Passenger.create()
+3=Passenger.create()
+4=Passenger.create()
+5=Passenger.create()
+6=Passenger.create()
